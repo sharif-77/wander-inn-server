13192a467e78890592edb3d43351d32ba3aaf0f8
muhammad-sharif <muhammadsharif80400@gmail.com>
2023-11-05 17:48:51 +0600

    exclusive offers data sended to client

--- index.js
+++ index.js
@@ -5,15 +5,62 @@ var cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
 
-
 app.use(cors())
+app.use(express.json());
+
 app.use(cookieParser())
 
 
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.pkahof5.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const roomsCollection = client.db("WanderInn").collection("rooms");
+    const offersCollection = client.db("WanderInn").collection("Offers");
+
+    
+   app.get('/offers',async (req,res)=>{
+      const data =  offersCollection.find();
+      const result=await data.toArray()
+      res.send(result)
+    })
+
+
+
+
+
+
+
+
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`server is running on port: ${port}`);
+});
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
